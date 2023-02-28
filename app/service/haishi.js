692d3c3c55484b3620c808469bde0a312af7d3ca
1039874238 <1039874238@qq.com>
2023-02-28 18:00:29 +0800

    code

--- app/service/haishi.js
+++ app/service/haishi.js
@@ -3,22 +3,37 @@
 const Service = require('egg').Service;
 const dayjs = require('dayjs');
 
-
 class HaishiService extends Service {
   async addUser(params) {
-    await this.ctx.model.StudentsJshs.insertMany(params.students);
+    const userList = await this.ctx.model.StudentsJshs.find({
+      $or: [{ status: '0' }, { status: '1' }],
+    });
+    const newUserList = params.students.filter(item => {
+      const findUser = userList.find(value => value.idCard === item.idCard);
+      if (findUser && findUser.password === item.password) {
+        return false;
+      }
+      return true;
+
+    });
+    await this.ctx.model.StudentsJshs.insertMany(newUserList);
     this.ctx.body = {
       state: 200,
       msg: '新建成功',
     };
   }
   async addProject(params) {
-    const project = await this.ctx.model.ProjectJshs.find({ studentId: params.studentId, projectId: params.projectId });
+    const project = await this.ctx.model.ProjectJshs.find({
+      studentId: params.studentId,
+      projectId: params.projectId,
+    });
     let msg = '新建成功';
     if (project.length > 0) {
       msg = '已存在';
     } else {
-      await this.ctx.model.ProjectJshs.insertMany([{ ...params, startTime: dayjs().format('YYYY-MM-DD HH:mm:ss') }]);
+      await this.ctx.model.ProjectJshs.insertMany([
+        { ...params, startTime: dayjs().format('YYYY-MM-DD HH:mm:ss') },
+      ]);
     }
     this.ctx.body = {
       state: 200,
@@ -47,7 +62,9 @@ class HaishiService extends Service {
   }
 
   async getProject(params) {
-    const output = await this.ctx.model.ProjectJshs.find({ studentId: params.studentId });
+    const output = await this.ctx.model.ProjectJshs.find({
+      studentId: params.studentId,
+    });
     this.ctx.body = {
       state: 200,
       data: output,
@@ -55,7 +72,9 @@ class HaishiService extends Service {
     };
   }
   async getUser(params) {
-    const output = await this.ctx.model.StudentsJshs.find({ status: params.status });
+    const output = await this.ctx.model.StudentsJshs.find({
+      status: params.status,
+    });
     this.ctx.body = {
       state: 200,
       data: output,
