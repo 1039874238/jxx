3ccb486a6ccc10b0c145e8f473b3dbec983e00cb
1039874238 <1039874237@qq.com>
2023-10-08 14:45:46 +0800

    调整定时任务通知

--- app/schedule/bot.js
+++ app/schedule/bot.js
@@ -6,12 +6,12 @@ const Subscription = require('egg').Subscription;
 class WatchBrowser extends Subscription {
   static get schedule() {
     return {
-      interval: '5m',
-      type: 'all'
-    }
+      interval: '30m',
+      type: 'all',
+    };
   }
   async subscribe() {
-    this.ctx.service.bot.checkBrowser()
+    this.ctx.service.bot.checkBrowser();
   }
 }
 module.exports = WatchBrowser;
