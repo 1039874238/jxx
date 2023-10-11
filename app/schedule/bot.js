a6e5f906a573f13a65fd86097d9889fc0d1c7d79
1039874238 <1039874237@qq.com>
2023-10-11 15:18:03 +0800

    定时器优化

--- app/schedule/bot.js
+++ app/schedule/bot.js
@@ -6,7 +6,7 @@ const Subscription = require('egg').Subscription;
 class WatchBrowser extends Subscription {
   static get schedule() {
     return {
-      interval: '30m',
+      interval: '5m',
       type: 'all',
     };
   }
