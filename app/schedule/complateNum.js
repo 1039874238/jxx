f2d8eddb6faa1d3ec5a37886fff1f2c1d641663a
1039874238 <1039874237@qq.com>
2023-10-13 11:03:47 +0800

    定时任务 ：15分钟执行一次

--- app/schedule/complateNum.js
+++ app/schedule/complateNum.js
@@ -6,7 +6,7 @@ const Subscription = require('egg').Subscription;
 class WatchComplate extends Subscription {
   static get schedule() {
     return {
-      cron: '0 0/1 * * * ?',
+      cron: '0 0/15 * * * ?',
       type: 'worker',
     };
   }
